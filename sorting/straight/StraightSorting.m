db7fe0e5cc0d00e51784c99105a167895dc3577d
Oleksii Palagin <apalagin@outlook.com>
2017-09-05 23:05:57 -0700

    Port StraightSorting to OBC

--- sorting/straight/StraightSorting.m
+++ sorting/straight/StraightSorting.m
@@ -1,22 +1,21 @@
-<*+ MAIN *>
 MODULE StraightSorting;
 
 IMPORT Out, Utils;
-  
-VAR 
+
+VAR
   a0, a1: ARRAY 10 OF INTEGER;
 
 
 (* 2.2.1 Sorting by Straight Insertion *)
 PROCEDURE InsertionSort(VAR a: ARRAY OF INTEGER);
-VAR 
+VAR
   x, i, j: INTEGER;
 BEGIN
   FOR i := 1 TO LEN(a) - 1 DO
-    x := a[i]; 
+    x := a[i];
     j := i;
     WHILE (j > 0) & (x < a[j - 1]) DO
-      a[j] := a[j - 1]; 
+      a[j] := a[j - 1];
       DEC(j);
     END;
     a[j] := x;
@@ -26,23 +25,23 @@ END InsertionSort;
 
 (* 2.2.1 Sorting by Binary Insertion *)
 PROCEDURE BinInsertionSort(VAR a: ARRAY OF INTEGER);
-VAR 
+VAR
   x, i, j, m, L, R: INTEGER;
 BEGIN
   FOR i := 1 TO LEN(a) - 1 DO
-    x := a[i]; 
-    L := 0; 
+    x := a[i];
+    L := 0;
     R := i;
     WHILE (L < R) DO
       m := (L + R)DIV 2;
-      IF (x >= a[m]) THEN 
-        L := m + 1; 
-      ELSE 
-        R := m 
+      IF (x >= a[m]) THEN
+        L := m + 1;
+      ELSE
+        R := m
       END;
     END;
-    FOR j := i TO R + 1 BY - 1 DO 
-      a[j] := a[j - 1]; 
+    FOR j := i TO R + 1 BY - 1 DO
+      a[j] := a[j - 1];
     END;
     a[R] := x;
   END;
@@ -53,19 +52,19 @@ END BinInsertionSort;
 PROCEDURE SelectionSort(VAR a: ARRAY OF INTEGER);
 VAR
   x: INTEGER;
-  i, j, k, N: LONGINT;
+  i, j, k, N: INTEGER;
 BEGIN
   N := LEN(a);
   FOR i := 0 TO N - 2 DO
-    k := i; 
+    k := i;
     x := a[i];
     FOR j := i + 1 TO N - 1 DO
-      IF a[j] < x THEN 
-        k := j; 
+      IF a[j] < x THEN
+        k := j;
         x := a[k]
       END
     END;
-    a[k] := a[i]; 
+    a[k] := a[i];
     a[i] := x
   END
 END SelectionSort;
@@ -75,7 +74,7 @@ END SelectionSort;
 PROCEDURE BubbleSort(VAR a: ARRAY OF INTEGER);
 VAR
   x: INTEGER;
-  i, j, N: LONGINT;
+  i, j, N: INTEGER;
 BEGIN
   N := LEN(a);
   FOR i := 1 TO N - 1 DO
@@ -91,45 +90,45 @@ END BubbleSort;
 
 
 BEGIN
-  a0[0] := 6; 
-  a0[1] := 9; 
-  a0[2] := 1; 
-  a0[3] := 2; 
-  a0[4] := 4; 
-  a0[5] := 3; 
-  a0[6] := 7; 
-  a0[7] := 5; 
-  a0[8] := 8; 
+  a0[0] := 6;
+  a0[1] := 9;
+  a0[2] := 1;
+  a0[3] := 2;
+  a0[4] := 4;
+  a0[5] := 3;
+  a0[6] := 7;
+  a0[7] := 5;
+  a0[8] := 8;
   a0[9] := 0;
-  
+
   Out.String("--- Straight Insertion Sort ---"); Out.Ln();
   Utils.CopyArray(a0, a1);
   Out.String("Unsorted = "); Utils.PrintArray(a1); Out.Ln();
   InsertionSort(a1);
   Out.String("Sorted = "); Utils.PrintArray(a1); Out.Ln();
-  
+
   Out.Ln();
-  
+
   Out.String("--- Binary Insertion Sort ---"); Out.Ln();
   Utils.CopyArray(a0, a1);
   Out.String("Unsorted = "); Utils.PrintArray(a1); Out.Ln();
   BinInsertionSort(a1);
   Out.String("Sorted = "); Utils.PrintArray(a1); Out.Ln();
-  
+
   Out.Ln();
-    
+
   Out.String("--- Straight Selection Sort ---"); Out.Ln();
   Utils.CopyArray(a0, a1);
   Out.String("Unsorted = "); Utils.PrintArray(a1); Out.Ln();
   SelectionSort(a1);
   Out.String("Sorted = "); Utils.PrintArray(a1); Out.Ln();
-    
+
   Out.Ln();
-      
+
   Out.String("--- Straight Exchange Sort ---"); Out.Ln();
   Utils.CopyArray(a0, a1);
   Out.String("Unsorted = "); Utils.PrintArray(a1); Out.Ln();
   BubbleSort(a1);
   Out.String("Sorted = "); Utils.PrintArray(a1); Out.Ln();
-    
+
 END StraightSorting.
