086e6a43d79edd92519a4ccb0a154acf78b79048
Oleksii Palagin <apalagin@outlook.com>
2017-09-06 08:04:18 -0700

    Port advanced sorting to OBC

--- sorting/advanced/AdvancedSorting.m
+++ sorting/advanced/AdvancedSorting.m
@@ -1,23 +1,22 @@
-<*+ MAIN *>
 MODULE AdvancedSorting;
 
 IMPORT Out, Utils;
 
-VAR 
+VAR
   a0, a1: ARRAY 10 OF INTEGER;
-  k: LONGINT;
+  k: INTEGER;
 
 (* Insertion Sort By Diminishing Increment (ShellSort) *)
 PROCEDURE ShellSort(VAR a: ARRAY OF INTEGER);
-CONST 
+CONST
   T = 4;
 VAR
-  i, j, N: LONGINT;
+  i, j, N: INTEGER;
   k, m, x: INTEGER;
   h: ARRAY T OF INTEGER;
 BEGIN
   N := LEN(a);
-  h[0] := 9; 
+  h[0] := 9;
   h[1] := 5;
   h[2] := 3;
   h[3] := 1;
@@ -26,12 +25,12 @@ BEGIN
     FOR i := k TO N - 1 DO
       x := a[i];
       j := i - k;
-      
+
       WHILE (j >= k) & (x < a[j]) DO
         a[j + k] := a[j];
-        j := j - k; 
+        j := j - k;
       END;
-      
+
       IF (j >= k)OR(x >= a[j]) THEN
         a[j + k] := x
       ELSE
@@ -46,22 +45,22 @@ END ShellSort;
 (* 2.3.2 Tree Sort (Heapsort) *)
 PROCEDURE HeapSort(VAR a: ARRAY OF INTEGER);
 VAR
-  N, L, R: LONGINT;
+  N, L, R: INTEGER;
   x: INTEGER;
 
-  PROCEDURE sift(L, R: LONGINT);
-  VAR 
-    i, j: LONGINT;
+  PROCEDURE sift(L, R: INTEGER);
+  VAR
+    i, j: INTEGER;
     x: INTEGER;
   BEGIN
     i := L;
     j := 2 * i + 1;
     x := a[i];
-  
+
     IF (j < R) & (a[j] < a[j + 1]) THEN
       INC(j);
     END;
-  
+
     WHILE (j <= R) & (x < a[j]) DO
       a[i] := a[j];
       i := j; j := 2 * j + 1;
@@ -69,7 +68,7 @@ VAR
         INC(j);
       END;
     END;
-  
+
     a[i] := x;
   END sift;
 
@@ -77,12 +76,12 @@ BEGIN
   N := LEN(a);
   L := N DIV 2;
   R := N - 1;
-  
+
   WHILE L > 0 DO
     DEC(L);
     sift(L, R);
   END;
-  
+
   WHILE R > 0 DO
     x := a[0]; a[0] := a[R]; a[R] := x;
     DEC(R);
@@ -93,23 +92,23 @@ END HeapSort;
 
 (* 2.3.3 Partition Sort (Quicksort) *)
 PROCEDURE QuickSort(VAR a: ARRAY OF INTEGER);
-VAR 
-  N: LONGINT;
-  
-  PROCEDURE sort(L, R: LONGINT);
+VAR
+  N: INTEGER;
+
+  PROCEDURE sort(L, R: INTEGER);
   VAR
-    i, j: LONGINT;
+    i, j: INTEGER;
     x, w: INTEGER;
   BEGIN
     i := L; j := R;
     (* partition *)
     x := a[(L + R) DIV 2];
     REPEAT
-      WHILE a[i] < x DO 
-        INC(i); 
+      WHILE a[i] < x DO
+        INC(i);
       END;
-      WHILE x < a[j] DO 
-        DEC(j); 
+      WHILE x < a[j] DO
+        DEC(j);
       END;
       IF (i <= j) THEN
         w := a[i]; a[i] := a[j]; a[j] := w;
@@ -117,11 +116,11 @@ VAR
       END;
     UNTIL i > j;
     (* recursively sort partitions *)
-    IF (L < j) THEN 
-      sort(L, j); 
+    IF (L < j) THEN
+      sort(L, j);
     END;
-    IF (i < R) THEN 
-      sort(i, R); 
+    IF (i < R) THEN
+      sort(i, R);
     END;
   END sort;
 
@@ -135,10 +134,10 @@ END QuickSort;
 PROCEDURE NRQuickSort(VAR a: ARRAY OF INTEGER);
 CONST M = 12; (* OP: this can be computed as log(N) *)
   VAR
-    i, j, L, R, s, N: LONGINT;
+    i, j, L, R, s, N: INTEGER;
     x, w: INTEGER;
-    low, high: ARRAY M OF LONGINT; (* index stack *)
-BEGIN 
+    low, high: ARRAY M OF INTEGER; (* index stack *)
+BEGIN
   N := LEN(a);
   s := 0; low[0] := 0; high[0] := N - 1;
   REPEAT (* take top request from stack *)
@@ -163,20 +162,20 @@ END NRQuickSort;
 
 
 (* 2.3.4 Finding the Media (k-th smallest of n items) *)
-PROCEDURE Find(VAR a: ARRAY OF INTEGER; k: LONGINT);
-VAR 
-  i, j, N, L, R: LONGINT;
+PROCEDURE Find(VAR a: ARRAY OF INTEGER; k: INTEGER);
+VAR
+  i, j, N, L, R: INTEGER;
   x, w: INTEGER;
 BEGIN
   N := LEN(a);
   L := 0; R := N - 1;
-  WHILE L < R - 1 DO 
+  WHILE L < R - 1 DO
     x := a[k]; i := L; j := R;
     REPEAT
-      WHILE a[i] < x DO 
+      WHILE a[i] < x DO
         INC(i)
       END;
-      WHILE x < a[j]DO 
+      WHILE x < a[j]DO
         DEC(j)
       END;
       IF i <= j THEN
@@ -184,61 +183,60 @@ BEGIN
         INC(i); DEC(j);
       END;
     UNTIL i > j;
-    IF j < k THEN 
-      L := i 
+    IF j < k THEN
+      L := i
     END;
-    IF k < i THEN 
-      R := j 
-    END; 
+    IF k < i THEN
+      R := j
+    END;
   END;
 END Find;
 
 (* MAIN *)
 BEGIN
-  a0[0] := 6; 
-  a0[1] := 9; 
-  a0[2] := 1; 
-  a0[3] := 2; 
-  a0[4] := 4; 
-  a0[5] := 3; 
-  a0[6] := 7;  
-  a0[7] := 5; 
-  a0[8] := 8; 
+  a0[0] := 6;
+  a0[1] := 9;
+  a0[2] := 1;
+  a0[3] := 2;
+  a0[4] := 4;
+  a0[5] := 3;
+  a0[6] := 7;
+  a0[7] := 5;
+  a0[8] := 8;
   a0[9] := 0;
-    
+
   Out.String("--- Shell Sort ---"); Out.Ln();
   Utils.CopyArray(a0, a1);
   Out.String("Unsorted = "); Utils.PrintArray(a1); Out.Ln();
   ShellSort(a1);
-  Out.String("Sorted = "); Utils.PrintArray(a1); Out.Ln(); 
-  
+  Out.String("Sorted = "); Utils.PrintArray(a1); Out.Ln();
+
   Out.Ln();
   Out.String("--- Heap Sort ---"); Out.Ln();
   Utils.CopyArray(a0, a1);
   Out.String("Unsorted = "); Utils.PrintArray(a1); Out.Ln();
   HeapSort(a1);
   Out.String("Sorted = "); Utils.PrintArray(a1); Out.Ln();
-  
+
   Out.Ln();
   Out.String("--- Quick Sort ---"); Out.Ln();
   Utils.CopyArray(a0, a1);
   Out.String("Unsorted = "); Utils.PrintArray(a1); Out.Ln();
   QuickSort(a1);
   Out.String("Sorted = "); Utils.PrintArray(a1); Out.Ln();
-  
+
   Out.Ln();
   Out.String("--- Non-recursive Quick Sort ---"); Out.Ln();
   Utils.CopyArray(a0, a1);
   Out.String("Unsorted = "); Utils.PrintArray(a1); Out.Ln();
   NRQuickSort(a1);
   Out.String("Sorted = "); Utils.PrintArray(a1); Out.Ln();
-  
+
   Out.Ln();
   Out.String("--- Finding median ---"); Out.Ln();
   Utils.CopyArray(a0, a1);
   Out.String("Unsorted = "); Utils.PrintArray(a1); Out.Ln();
   k := LEN(a1) DIV 2;
   Out.String("Median = "); Find(a1, k); Out.Int(a1[k], 0); Out.Ln();
-      
-END AdvancedSorting.
 
+END AdvancedSorting.
