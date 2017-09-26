981ad352e8d3ce1ac2f081904465b8c9d9a22b77
Oleksii Palagin <opalagin@users.noreply.github.com>
2017-09-26 08:06:30 -0700

    Refactor substring search for OBC

--- substring/StringSearch.m
+++ substring/StringSearch.m
@@ -1,118 +1,104 @@
-<*+ MAIN *>
 MODULE StringSearch;
 
-IMPORT Out;
+IMPORT Out, Strings;
 
 (* Strings are NULL-terminated *)
-TYPE 
+TYPE
   String = ARRAY 255 OF CHAR;
-  
+
 VAR
   i: INTEGER;
   test, a, b: String;
 
-(* Length of NULL-terminated string *)  
-PROCEDURE Length(src: String): INTEGER;
-VAR i: INTEGER;
-BEGIN
-  i := 0;
-  WHILE (i < 255) & (ORD(src[i]) # 0H) DO
-    INC(i)
-  END;
-  
-  RETURN i
-END Length;
-
-
 (* 1.9.1: Straight String Search *)
 PROCEDURE IndexOf(p, s: String): INTEGER;
-VAR 
+VAR
   i, j, N, M: INTEGER;
 BEGIN
   i := 0;
   j := 0;
-  N := Length(s);
-  M := Length(p);
-  
+  N := Strings.Length(s);
+  M := Strings.Length(p);
+
   WHILE (i <= N - M) & (j < M) DO
     IF s[i + j] = p[j] THEN
       INC(j);
-    ELSE 
+    ELSE
       INC(i);
       j := 0;
     END
   END;
-  
+
   IF i > N - M THEN
     RETURN -1
   END;
-  
+
   RETURN i
 END IndexOf;
 
 
 (* 1.9.2 The Knuth-Morris-Pratt String Search *)
-PROCEDURE KMP_IndexOf(p, s: String): INTEGER;
+PROCEDURE KMPIndexOf(p, s: String): INTEGER;
 VAR
   i, j, k, N, M: INTEGER;
   d: POINTER TO ARRAY OF INTEGER;
 BEGIN
-  N := Length(s);
-  M := Length(p);
+  N := Strings.Length(s);
+  M := Strings.Length(p);
   NEW(d, M);
-  
+
   (* compute shifts *)
   d[0] := -1;
-  IF p[0] # p[1]THEN 
-    d[1] := 0 
-  ELSE 
-    d[1] := -1 
-  END; 
-  j := 1; 
+  IF p[0] # p[1]THEN
+    d[1] := 0
+  ELSE
+    d[1] := -1
+  END;
+  j := 1;
   k := 0;
   WHILE j < M - 1 DO
     IF (k >= 0) & (p[j] # p[k]) THEN
       k := d[k];
     ELSE
-      INC(j); 
+      INC(j);
       INC(k);
-      IF p[j] # p[k] THEN 
-        d[j] := k 
-      ELSE 
+      IF p[j] # p[k] THEN
+        d[j] := k
+      ELSE
         d[j] := d[k]
       END;
     END;
   END;
-  
+
   (* scan with shifts *)
-  i := 0; 
+  i := 0;
   j := 0;
   WHILE (j < M) & (i < N) DO
     IF (j >= 0) & (s[i] # p[j]) THEN
       j := d[j];
     ELSE
-      INC(i); 
+      INC(i);
       INC(j);
     END;
   END;
-  
+
   IF j = M THEN
     RETURN i - M
   END;
-  
+
   RETURN -1
-END KMP_IndexOf;
+END KMPIndexOf;
 
 
 (* 1.9.3 Simplfied Boyer-Moore String Search *)
-PROCEDURE BM_IndexOf(p, s: String): INTEGER;
+PROCEDURE BMIndexOf(p, s: String): INTEGER;
 VAR
   i, j, k, M, N: INTEGER;
   d: POINTER TO ARRAY OF INTEGER;
 BEGIN
-  M := Length(p);
-  N := Length(s);
-  
+  M := Strings.Length(p);
+  N := Strings.Length(s);
+
   (* compute shifts *)
   NEW(d, 128);
   FOR i := 0 TO 127 DO
@@ -121,7 +107,7 @@ BEGIN
   FOR j := 0 TO M-2 DO
     d[ORD(p[j])] := M - j - 1;
   END;
-  
+
   (* scan with shifts *)
   i := M;
   j := M;
@@ -136,13 +122,13 @@ BEGIN
         k := i;
     END;
   END;
-  
+
   IF j <= 0 THEN
     RETURN k
   END;
-  
+
   RETURN -1
-END BM_IndexOf;
+END BMIndexOf;
 
 
 (* MAIN *)
@@ -158,7 +144,7 @@ BEGIN
     test[8]  := 'o';
     test[9]  := 'l';
     test[10] := 'a';
-    test[11] := ' '; 
+    test[11] := ' ';
     test[12] := 'g';
     test[13] := 'i';
     test[14] := 'r';
@@ -180,7 +166,7 @@ BEGIN
     test[30] := 'n';
     test[31] := '.';
     test[32] := CHR(0);
-    
+
     a[0] := 'H';
     a[1] := 'o';
     a[2] := 'o';
@@ -190,36 +176,36 @@ BEGIN
     a[6] := 'a';
     a[7] := 'n';
     a[8] := CHR(0);
-    
+
     b[0] := 'H';
     b[1] := 'o';
     b[2] := 'l';
     b[3] := 'a';
     b[4] := CHR(0);
-    
+
     Out.String("S = ");
     Out.String(test);
     Out.Ln();
-    
+
     Out.String("Straight indexOf('"); Out.String(a); Out.String("'): ");
-    Out.Int(IndexOf(a, test), 0); 
+    Out.Int(IndexOf(a, test), 0);
     Out.Ln();
     Out.String("Straight indexOf('"); Out.String(b); Out.String("'): ");
     Out.Int(IndexOf(b, test), 0);
     Out.Ln();
-    
+
     Out.String("KMP indexOf('"); Out.String(a); Out.String("'): ");
-    Out.Int(KMP_IndexOf(a, test), 0); 
+    Out.Int(KMPIndexOf(a, test), 0);
     Out.Ln();
     Out.String("KMP indexOf('"); Out.String(b); Out.String("'): ");
-    Out.Int(KMP_IndexOf(b, test), 0);
+    Out.Int(KMPIndexOf(b, test), 0);
     Out.Ln();
-    
+
     Out.String("BM indexOf('"); Out.String(a); Out.String("'): ");
-    Out.Int(BM_IndexOf(a, test), 0); 
+    Out.Int(BMIndexOf(a, test), 0);
     Out.Ln();
     Out.String("BM indexOf('"); Out.String(b); Out.String("'): ");
-    Out.Int(BM_IndexOf(b, test), 0);
+    Out.Int(BMIndexOf(b, test), 0);
     Out.Ln();
-    
+
 END StringSearch.
